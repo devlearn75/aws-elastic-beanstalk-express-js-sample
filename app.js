e064cadad545a82baa4ac556cd1060476bb93fb8
Devender Sharma <devender@Devenders-MacBook-Air.local>
2020-12-19 11:22:31 +0530

    change 3@11:21

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!...from aws@19-dec-2020.. added manual review between build & deploy'));
+app.get('/', (req, res) => res.send('Hello World!...from aws@19-dec-2020.. change 3 @ 11:21'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
