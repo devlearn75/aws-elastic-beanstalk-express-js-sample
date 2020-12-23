682c0347298dae53dc09cf8d2eed9d2515245fdf
Devender Sharma <devender@Devenders-MacBook-Air.local>
2020-12-23 11:58:12 +0530

    23-dec-change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!...from aws@19-dec-2020.. change 5 @ 18:00'));
+app.get('/', (req, res) => res.send('23-Dec-2020 Hello World!...from aws@19-dec-2020.. change 5 @ 18:00'));
 
 
 //app.getfor('/', (req, res) => res.send('Hello World!...from aws@19-dec-2020.. change 4 @ 11:26'));
